3abbd0ccd7add196d6d76ba0214adca41a4926e7
00466612 <00466612@FMTI.Forsvaret.Local>
2018-08-21 16:45:54 +0200

    added map

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 var data = {"layers":{"layer":[{"name":"Ansøgere10","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere10.json"},{"name":"Ansøgere11","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere11.json"},{"name":"Ansøgere12","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere12.json"},{"name":"Ansøgere13","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere13.json"},{"name":"Ansøgere14","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere14.json"},{"name":"Ansøgere15","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere15.json"},{"name":"Ansøgere16","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere16.json"},{"name":"Ansøgere17","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gere17.json"},{"name":"CHR00","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR00.json"},{"name":"CHR01","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR01.json"},{"name":"CHR02","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR02.json"},{"name":"CHR03","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR03.json"},{"name":"CHR04","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR04.json"},{"name":"CHR05","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR05.json"},{"name":"CHR06","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR06.json"},{"name":"CHR07","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR07.json"},{"name":"CHR08","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR08.json"},{"name":"CHR09","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR09.json"},{"name":"CHR10","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR10.json"},{"name":"CHR11","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR11.json"},{"name":"CHR12","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR12.json"},{"name":"CHR13","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR13.json"},{"name":"CHR14","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR14.json"},{"name":"CHR15","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR15.json"},{"name":"CHR17","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR17.json"},{"name":"CHRsogn","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHRsogn.json"},{"name":"CHRsogn0015","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHRsogn0015.json"},{"name":"DE_udvikling_oplande_07til14","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/DE_udvikling_oplande_07til14.json"},{"name":"HusdyrtrykOplande2007Til2015","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/HusdyrtrykOplande2007Til2015.json"},{"name":"AktionsomraaderNaturpakkeJordfordelinger","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/AktionsomraaderNaturpakkeJordfordelinger.json"},{"name":"AnsøgereSogn2010Til2015","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gereSogn2010Til2015.json"},{"name":"AnsøgereSogn2010Til2016","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gereSogn2010Til2016.json"},{"name":"AnsøgereSogn2010Til2017","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Ans%C3%B8gereSogn2010Til2017.json"},{"name":"CHR16","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHR16.json"},{"name":"CHRsogn2000Til2016","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHRsogn2000Til2016.json"},{"name":"CHRsogn2000Til2017","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/CHRsogn2000Til2017.json"},{"name":"HusdyrtrykOplande2007Til2016","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/HusdyrtrykOplande2007Til2016.json"},{"name":"MarkblokkeSogn2007til2016","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/MarkblokkeSogn2007til2016.json"},{"name":"MarkblokkeSogn2007til2017","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/MarkblokkeSogn2007til2017.json"},{"name":"OmdriftsarealerSogn2010Til2016","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/OmdriftsarealerSogn2010Til2016.json"},{"name":"OmdriftsarealerSogn2010Til2017","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/OmdriftsarealerSogn2010Til2017.json"},{"name":"ProjektjordfordelingerNaturpakken","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/ProjektjordfordelingerNaturpakken.json"},{"name":"Jordbundskort2014","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Jordbundskort2014.json"},{"name":"MarkblokkeSogn2007Til2015","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/MarkblokkeSogn2007Til2015.json"},{"name":"Marker12","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Marker12.json"},{"name":"Marker13","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Marker13.json"},{"name":"Marker14","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Marker14.json"},{"name":"Marker15","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Marker15.json"},{"name":"Marker16","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Marker16.json"},{"name":"Marker17","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Marker17.json"},{"name":"Oekomarker2015","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Oekomarker2015.json"},{"name":"OmdriftsarealerSogn2010Til2015","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/OmdriftsarealerSogn2010Til2015.json"},{"name":"AktionsomraaderNaturpakke","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/AktionsomraaderNaturpakke.json"},{"name":"Forkoebsret på arealer til salg","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Forkoebsret+p%C3%A5+arealer+til+salg.json"},{"name":"Projektgraenser for arealer til salg","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/Projektgraenser+for+arealer+til+salg.json"},{"name":"ProjektomraaderNaturpakken","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/ProjektomraaderNaturpakken.json"},{"name":"hopltest2018","href":"http:\/\/jordbrugsanalyser.dk\/geoserver\/rest\/layers\/hopltest2018.json"}]}}
 
-const sidebar = document.querySelector("sidebar");
 const leftmenu = document.querySelector("leftmenu");
-/*     sidebar.addEventListener("click",(e)=>{
-        if(leftmenu.style.display !== "none"){ 
-            leftmenu.style.display = "none";
-        }
-        else{
-            leftmenu.style.display = "block";
-        }
-       // map.resize();
-     })
 
-     mapboxgl.accessToken = 'pk.eyJ1IjoidGlub2tzIiwiYSI6Ikp4OE0yWjQifQ.8ShzvCuk6zpjf9n_1pS_fA';
+mapboxgl.accessToken = 'pk.eyJ1IjoidGlub2tzIiwiYSI6Ikp4OE0yWjQifQ.8ShzvCuk6zpjf9n_1pS_fA';
      var map = new mapboxgl.Map({
          container: 'map', // container id
-         style: 'mapbox://styles/mapbox/streets-v9', // stylesheet location
-         center: [-74.50, 40], // starting position [lng, lat]
-         zoom: 9 // starting zoom
+         style: 'mapbox://styles/tinoks/cjibgapw20h092sp11g1erri7', // stylesheet location
+         center: [0, 20], // starting position [lng, lat]
+         zoom: 2// starting zoom
      });
+     document.querySelector(".mapboxgl-control-container").remove();
+     
 
-     */
-
-    var setCheck;
+    var activeSidetab;
     [...document.getElementsByTagName("input")].forEach((e)=>{
 
         e.addEventListener('click',  function(e){
+            if(activeSidetab){
+                document.querySelector("leftmenu "+ activeSidetab.id).style.display = "none"
+            } 
             
-            if(setCheck != this){
-                setCheck = this;
+            if(activeSidetab != this){
+                activeSidetab = this;
+                leftmenu.style.display = "block";
+                document.querySelector("leftmenu "+activeSidetab.id).style.display = "flex"
             }
             else{
                 this.checked = false;
-                setCheck = null;
+                activeSidetab = null;
+                leftmenu.style.display = "none";
             }
-            console.log(setCheck)
+            map.resize();
         });
     })
